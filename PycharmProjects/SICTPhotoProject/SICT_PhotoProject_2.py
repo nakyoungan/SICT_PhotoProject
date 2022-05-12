165d5aa5a89c4ad608918efa79c81b32255c7b9d
pepperdad <jeong20909@naver.com>
2022-05-12 12:40:44 +0900

    Second Commit | 프레임 위에 4사진 4장 덮어쓰기 구현

--- PycharmProjects/SICTPhotoProject/SICT_PhotoProject_2.py
+++ PycharmProjects/SICTPhotoProject/SICT_PhotoProject_2.py
@@ -30,12 +30,12 @@ cv2.destroyAllWindows()
 result_width = 800
 result_height = 2700
 
-input_path_1 = r'C:\Users\USER\PycharmProjects\SICTPhotoProject\save_image.jpg'
+input_path_1 = r'C:\Users\USER\PycharmProjects\SICTPhotoProject\save_image.jpg' # 덮어쓸 사진의 경로
 result = Image.new("L",(result_width, result_height))
 input_img_1 = Image.open(r'C:\Users\USER\PycharmProjects\SICTPhotoProject\save_image.jpg')
-result.paste(im=input_img_1, box=(80, 80))
-result.paste(im=input_img_1, box=(80, 640))
-result.paste(im=input_img_1, box=(80, 1200))
-result.paste(im=input_img_1, box=(80, 1760))
+result.paste(im=input_img_1, box=(80, 80)) # 1번째 사진 덮어쓰기
+result.paste(im=input_img_1, box=(80, 640)) # 2번째 사진 덮어쓰기
+result.paste(im=input_img_1, box=(80, 1200)) # 3번째 사진 덮어쓰기
+result.paste(im=input_img_1, box=(80, 1760)) # 4번째 사진 덮어쓰기
 
-result.save(r'C:\Users\USER\PycharmProjects\SICTPhotoProject\result_image.jpg')
+result.save(r'C:\Users\USER\PycharmProjects\SICTPhotoProject\result_image.jpg') # 사진이 저장될 경로
