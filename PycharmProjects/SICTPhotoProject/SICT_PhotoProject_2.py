9a3b3874064a78ddd782355e0de0a1c760c9da3f
jeong20909 <jeong20909@naver.com>
2022-07-01 12:42:39 +0900

    Feat: 프레임에 맞춰서 사진 4장을 저장한다

--- PycharmProjects/SICTPhotoProject/SICT_PhotoProject_2.py
+++ PycharmProjects/SICTPhotoProject/SICT_PhotoProject_2.py
@@ -1,41 +1,62 @@
 import cv2
+import time
+import os
 from PIL import Image
 
+print('스융네컷 가즈아')
 
 #동영상에서 사진 찍는
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3, 720)
 cap.set(4, 1080)
 fc = 20.0
 codec = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
 out = cv2.VideoWriter('mycam.avi', codec, fc, (int(cap.get(3)), int(cap.get(4))))
+
+n = 1
 while True:
     ret, frame = cap.read()
     cv2.imshow('test', frame)
     out.write(frame)
-    k = cv2.waitKey(1)
+    k = cv2.waitKey(5)
 
-    if cv2.waitKey(1) == ord('q'):
-        cv2.imwrite('save_image.jpg', frame)
-
+    if (n == 5):
         break
+    if k == ord('q'):
+        cv2.imwrite('save_image' + str(n) + '.jpg', frame)
+        print(str(n) + '번째 사진')
+        n += 1
 
     if k == 27:
         break
 
-cap.release()
-
-cv2.destroyAllWindows()
-
 result_width = 800
 result_height = 2700
 
-input_path_1 = r'C:\Users\USER\PycharmProjects\SICTPhotoProject\save_image.jpg' # 덮어쓸 사진의 경로
-result = Image.new("L",(result_width, result_height))
-input_img_1 = Image.open(r'C:\Users\USER\PycharmProjects\SICTPhotoProject\save_image.jpg')
-result.paste(im=input_img_1, box=(80, 80)) # 1번째 사진 덮어쓰기
-result.paste(im=input_img_1, box=(80, 640)) # 2번째 사진 덮어쓰기
-result.paste(im=input_img_1, box=(80, 1200)) # 3번째 사진 덮어쓰기
-result.paste(im=input_img_1, box=(80, 1760)) # 4번째 사진 덮어쓰기
+#result = Image.new("L", (result_width, result_height))
+result = Image.open('./damgom.jpg') # 프레임
 
-result.save(r'C:\Users\USER\PycharmProjects\SICTPhotoProject\result_image.jpg') # 사진이 저장될 경로
+input_img_1 = Image.open('./save_image1.jpg') # 덮어쓸 사진의 경로
+input_img_2 = Image.open('./save_image2.jpg') # 덮어쓸 사진의 경로
+input_img_3 = Image.open('./save_image3.jpg') # 덮어쓸 사진의 경로
+input_img_4 = Image.open('./save_image4.jpg') # 덮어쓸 사진의 경로
+
+result.paste(im=input_img_1, box=(80, 264))
+result.paste(im=input_img_2, box=(80, 798))
+result.paste(im=input_img_3, box=(80, 1332))
+result.paste(im=input_img_4, box=(80, 1866))
+
+
+result_count = 0
+
+while(1):
+    if(not(os.path.isfile('./result_image'+ str(result_count) + '.jpg'))):
+        print('./result_image'+ str(result_count) + '.jpg')
+        result.save('./result_image'+ str(result_count) + '.jpg') # 사진이 저장될 경로
+        break
+    else:
+        result_count += 1
+        print("Result : " + str(result_count))
+
+cap.release()
+cv2.destroyAllWindows()
